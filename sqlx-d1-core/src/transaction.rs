032ee2cfd71a345b13f7d6121aff19fc74e1fe23
kanarus <mail@kanarus.dev>
2026-04-13 03:50:23 +0900

    fix fmt

--- sqlx-d1-core/src/transaction.rs
+++ sqlx-d1-core/src/transaction.rs
@@ -8,7 +8,8 @@ impl sqlx_core::transaction::TransactionManager for D1TransactionManager {
     type Database = crate::D1;
 
     fn begin<'conn>(
-        #[allow(unused)] conn: &'conn mut <Self::Database as sqlx_core::database::Database>::Connection,
+        #[allow(unused)]
+        conn: &'conn mut <Self::Database as sqlx_core::database::Database>::Connection,
         #[allow(unused)] statement: Option<Cow<'static, str>>,
     ) -> crate::ResultFuture<'conn, ()> {
         #[cfg(not(target_arch = "wasm32"))]
@@ -51,9 +52,7 @@ impl sqlx_core::transaction::TransactionManager for D1TransactionManager {
         /* do nothing */
     }
 
-    fn get_transaction_depth(
-        #[allow(unused)] conn: &D1Connection,
-    ) -> usize {
-        1  // dummy since this will never be used because d1 does not support transactions
+    fn get_transaction_depth(#[allow(unused)] conn: &D1Connection) -> usize {
+        1 // dummy since this will never be used because d1 does not support transactions
     }
 }
